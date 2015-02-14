13664e16f958fd3ef5dd7fea2a79e0439bb49b1b
JoJoPuppe <marcusloeper@gmx.de>
2015-02-14 09:05:10 +0100

    update

--- src/scripts/actions.py
+++ src/scripts/actions.py
@@ -2,11 +2,7 @@ def start():
 	cards = table.create('31000a40-f835-4d50-8e76-d41266250004', 6, 100, quantity = 6, persist = True)
 	for card in cards:
     	notify("{} created {}.".format(me, card))
-    	cardsR = table.create('31000a40-f835-4d50-8e76-d41266250011', 16, 110, quantity = 6, persist = True)
-	for card in cards:
-    	notify("{} created {}.".format(me, card))
     	cards.moveTo(me.hand)
-    	cardsR.moveTo(me.hand)
 
 def sitstand(group, x = 0, y = 0):
     isstanding = me.getGlobalVariable("standing")
